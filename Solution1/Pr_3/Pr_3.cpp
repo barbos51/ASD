31679c754e52873a6f3036719ea654efd795d6e9
barbos51 <ivanovicbarbos51@gmail.com>
2024-03-05 10:34:34 +0200

    Pr_3

---
+++ Solution1/Pr_3/Pr_3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int a = 10;
+	int arr[a];
+	for (int i = 0; i < a; i++)
+	{
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+
+	int m;//число
+	int k;//перевірка для закінчення
+	while (true)
+	{
+		cout << "Enter number: ";
+		cin >> m;
+		for (int i = 0; i < a ; i++)
+		{
+			if (arr[i] == m)
+			{
+				cout << "arr[" << i << "] = " << arr[i]<<endl;
+				
+			}
+			
+		}
+		cout << "do you want to continue? enter 1 - if no, 0 - if yes:  ";
+		cin >> k;
+		if (k == 1)
+		{
+			break;
+		}
+	}
+}
