22171ec0b4a008b0bd92de6f8e4c39427f07beef
barbos51 <ivanovicbarbos51@gmail.com>
2024-03-05 10:34:34 +0200

    Pr_3

---
+++ Solution1/Pr_3/Pr_3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int a = 10;
+	int arr[a];
+	for (int i = 0; i < a; i++)
+	{
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+
+	int m;
+	while (true)
+	{
+		cout << "Enter number: ";
+		cin >> m;
+		for (int i = 0; i < a ; i++)
+		{
+			if (arr[i] == m)
+			{
+				cout << "arr[" << i << "] = " << arr[i]<<endl;
+				
+			}
+			
+		}
+	}
+}
